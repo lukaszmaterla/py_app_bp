e791dffe6bf82a8aaa26704197937beb38bd2346
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-15 19:59:00 +0200

    Added some condition to the project view

--- website/views.py
+++ website/views.py
@@ -5,7 +5,7 @@ from django.template.loader import get_template
 
 def home_page(request):
     my_title = 'hello there...'
-    return render(request, 'hello_word.html', {"title": my_title})
+    return render(request, 'home.html', {"title": my_title})
 
 
 def about(request):
