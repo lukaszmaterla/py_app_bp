f2b046b13b35836052dc69783533bfcfc24072e7
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-18 20:51:14 +0200

    create queryset with filter to get data as an array, get is use to select one object

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,11 @@ from .models import BlogPost
 
 
 def blog_post_detail_page(request, slug):
-    obj = get_object_or_404(BlogPost, slug=slug)
+    queryset = BlogPost.objects.filter(slug=slug)
+    if queryset.count() >= 1:
+        obj = queryset.first()
+
+    # obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
     context = {'obj': obj}
     return render(request, template_name, context)
