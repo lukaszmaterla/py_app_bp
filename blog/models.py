ee8e4cdb1399ef15c1233eae4e34197d78770e8e
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-18 20:54:28 +0200

    make slug unique

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 class BlogPost(models.Model):
     title = models.TextField()
-    slug = models.SlugField() # hello word -> hello-word
+    slug = models.SlugField(unique=True)  # hello word -> hello-word
     content = models.TextField(null=True, blank=True)
--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,9 @@ from .models import BlogPost
 
 def blog_post_detail_page(request, slug):
     queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() >= 1:
-        obj = queryset.first()
-
+    if queryset.count() == 0:
+        raise Http404
+    obj = queryset.first()
     # obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
     context = {'obj': obj}
