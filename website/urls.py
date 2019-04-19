5e4ebedc5746370826495b96a7d27ccdb5108418
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-19 19:08:54 +0200

    Seperate blog urls from project urls

--- website/urls.py
+++ website/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog.views import (
-    blog_post_detail_view,
-    blog_post_list_view,
     blog_post_create_view,
-    blog_post_update_view,
-    blog_post_delete_view
 )
 from .views import (
     home_page,
@@ -33,12 +29,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
     path('about/', about),
-    path('blog/', blog_post_list_view),
-    path('blog/<str:slug>/', blog_post_detail_view),
-    path('blog/<str:slug>/edit/', blog_post_update_view),
-    path('blog/<str:slug>/delete/', blog_post_delete_view),
+    path('blog/', include('blog.urls')),
     path('blog-new/', blog_post_create_view),
-
     path('contact/', contact),
     path('example/', example_page)
 ]
