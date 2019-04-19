c0b2209e5b74d921e1d1cf34171a89cfac730ff1
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-19 19:59:33 +0200

    CHanged blog post model and add some content to the title of blog post model

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from .forms import BlogPostForm
+from .forms import BlogPostForm, BlogPostModelForm
 from .models import BlogPost
 
 
@@ -23,10 +23,14 @@ def blog_post_list_view(request):
 
 
 def blog_post_create_view(request):
-    form = BlogPostForm(request.POST or None)
+    form = BlogPostModelForm(request.POST or None)
     if form.is_valid():
-        obj = BlogPost.objects.create(**form.cleaned_data)
-        form = BlogPostForm()
+
+        obj = form.save(commit=False)
+        obj.title = form.cleaned_data.get('title') + '-this is awesome'
+        form.save()
+
+        form = BlogPostModelForm()
     template_name = 'blog/form.html'
     context = {'form': form}
     return render(request, template_name, context)
