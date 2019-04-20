3bd4f3d52cb8f6fd06db3f4b57ca8e09108b3903
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-20 21:29:41 +0200

    Get user model from auth and create foreign key in blog post model

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
 class BlogPost(models.Model):
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)  # hello word -> hello-word
     content = models.TextField(null=True, blank=True)
