dd1a490f5d77a6d907d93edb2955ffc009971a71
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-20 21:31:38 +0200

    create some queryset for getting blogpost for spcified user

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class BlogPost(models.Model):
+    # get blog post for user query: blogpost_set() or BlogPost.objects.filter(user__id=id)
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)  # hello word -> hello-word
