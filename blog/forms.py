0213ecac6caeec7022f3fb6e1f933d474d0df0c9
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-22 14:57:04 +0200

    exclude title validation

--- blog/forms.py
+++ blog/forms.py
@@ -16,8 +16,12 @@ class BlogPostModelForm(forms.ModelForm):
         fields = ['title', 'slug', 'content']
 
     def clean_title(self, *args, **kwargs):
+        instance = self.instance
         title = self.cleaned_data.get('title')
         qs = BlogPost.objects.filter(title=title)
+
+        if instance is not None:
+            qs = qs.exclude(pk=instance.pk)
         if qs.exists():
             raise forms.ValidationError('Title already exists')
         return title
