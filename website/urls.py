7903da6447b197984c6ec9c064e09c72fede3e0c
lukaszmaterla <lukasz.materla91@gmail.com>
2019-04-26 20:54:45 +0200

    Defined static file configuration

--- website/urls.py
+++ website/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from blog.views import (
@@ -34,4 +35,9 @@ urlpatterns = [
     path('contact/', contact),
     path('example/', example_page)
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
